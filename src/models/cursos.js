9646e7f895a6856a03e01848e3e0f6d3f4b76266
David-forero <david.forero1813@gmail.com>
2020-06-08 20:38:12 -0400

    Se terminó de añadir los diseños de home, cursos y usuarios con efectos transi

---
+++ src/models/cursos.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const cursoSchema = ({
+    title: String,
+    description: String,
+    
+})
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -7,7 +7,22 @@ const { isAuthenticated, isAdmin } = require('../helpers/auth');
 
 router.get('/home', isAuthenticated, isAdmin, (req, res) =>{
     res.render('dashboard/index');
-    console.log('algo pasa')
+})
+
+router.get('/courses', isAuthenticated, isAdmin, (req, res) =>{
+    res.render('dashboard/cursos');
+})
+
+router.get('/news', isAuthenticated, isAdmin, (req, res) =>{
+    res.render('dashboard/index');
+})
+
+router.get('/events', isAuthenticated, isAdmin, (req, res) =>{
+    res.render('dashboard/index');
+})
+
+router.get('/users', isAuthenticated, isAdmin, (req, res) =>{
+    res.render('dashboard/users');
 })
 
 module.exports = router;
